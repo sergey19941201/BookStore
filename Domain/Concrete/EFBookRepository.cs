6838445872e5ad3bf22a57e0b5c3070d685c4963
sergey19941201 <koshelnick1994@mail.ru>
2017-02-10 22:03:56 +0200

    1st lesson well done

---
+++ Domain/Concrete/EFBookRepository.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Domain.Concrete
+{
+    public class EFBookRepository : IBookRepository
+    {
+        EFDbContext context = new EFDbContext();
+        public IEnumerable<Book> Books
+        {
+            get
+            {
+                return context.Books;
+            }
+        }
+    }
+}
---
+++ WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Abstract;
+using Domain.Concrete;
+using Domain.Entities;
+using Moq;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            kernel.Bind<IBookRepository>().To<EFBookRepository>();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+}

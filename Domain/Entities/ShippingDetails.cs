5011b56103276b735d012b584773070e23b759b2
sergey19941201 <koshelnick1994@mail.ru>
2017-02-17 04:43:58 +0200

    Started lesson 7 and stopped successfully on 2:00. Everything works.

---
+++ Domain/Entities/ShippingDetails.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class ShippingDetails
+    {
+        [Required(ErrorMessage="Укажите Ваше имя")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Адрес доставки")]
+        public string Line1 { get; set; }
+        public string Line2{ get; set; }
+        public string Line3{ get; set; }
+
+        [Required(ErrorMessage = "Укажите город")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Укажите страну")]
+        public string Country { get; set; }
+
+        public bool GiftWrap { get; set; }
+    }
+}
